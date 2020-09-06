5fdd35ff17c0d62a5e853671366a4ea717cdf4e5
altoid9091 <70575096+altoid9091@users.noreply.github.com>
2020-09-06 12:04:30 +0200

    Update PayCounter.py

--- PayCounter/PayCounter.py
+++ PayCounter/PayCounter.py
@@ -1,14 +1,10 @@
 #quick test
-print("Pay counter by altoid")
+print("Pay counter for retarted people")
 Er = input("Enter Pay: ")
 Fr = input("Enter Rate: ")
-try:
-    sh = float(Er)
-    tr = float(Fr)
-except:
-    print("Please Enter a number stupid")
-
 
 print("counting....")
 Xr = int(Er) * int(Fr)
 print("Your Pay:",Xr)
+f = input("quit?")
+quit()
